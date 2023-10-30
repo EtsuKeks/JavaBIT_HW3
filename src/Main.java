5ab41aff72b6730505599c3e010b8796b4eb1d5e
SexualWhiteChocolate <karimullin.tr@phystech.edu>
2023-10-30 04:17:51 +0300

    Ninth Commit

--- src/Main.java
+++ src/Main.java
@@ -7,73 +7,79 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
-//        HashSet<HashSet<Integer>> integer = new HashSet<>();
-//        HashSet<Integer> temp = new HashSet<>();
-//        temp.add(325);
-//        temp.add(3425);
-//        integer.add(temp);
-//        HashSet<Integer> temp2 = new HashSet<>();
-//        temp2.add(3235);
-//        temp2.add(2314);
-//        integer.add(temp2);
-//
-//        HashSet<HashSet<Integer>> integer2 = new HashSet<>();
-//        HashSet<Integer> temp3 = new HashSet<>();
-//        temp3.add(42545);
-//        temp3.add(34425);
-//        integer2.add(temp3);
-//        HashSet<Integer> temp4 = new HashSet<>();
-//        temp4.add(31342);
-//        temp4.add(24545);
-//        integer2.add(temp4);
-
-//        HashMap<Integer, HashSet<String>> integer = new HashMap<>();
-//        HashSet<String> temp1 = new HashSet<>();
-//        temp1.add("dwa");
-//        temp1.add("reerw");
-//        integer.put(3, temp1);
-//
-//        HashMap<Integer, HashSet<String>> integer2 = new HashMap<>();
-//        HashSet<String> temp2 = new HashSet<>();
-//        temp2.add("ealkdsr");
-//        temp2.add("dwagt");
-//        integer2.put(1, temp2);
-
+        HashSet<HashSet<Integer>> exmp1 = new HashSet<>();
         HashSet<Integer> temp1 = new HashSet<>();
-        temp1.add(5);
-        temp1.add(53);
-        HashMap<String, ArrayList<Integer>> temp2 = new HashMap<>();
+        temp1.add(325);
+        temp1.add(3425);
+        exmp1.add(temp1);
+        HashSet<Integer> temp2 = new HashSet<>();
+        temp2.add(3235);
+        temp2.add(2314);
+        exmp1.add(temp2);
+
+        HashSet<HashSet<Integer>> exmp2 = new HashSet<>();
+        HashSet<Integer> temp3 = new HashSet<>();
+        temp3.add(42545);
+        temp3.add(34425);
+        exmp2.add(temp3);
+        HashSet<Integer> temp4 = new HashSet<>();
+        temp4.add(31342);
+        temp4.add(24545);
+        exmp2.add(temp4);
+
+        Generator<HashSet<HashSet<Integer>>> generator1 = Factory.factoryToJSON(exmp1);
+        System.out.println(generator1.generate(exmp2));
+
+        HashMap<Integer, HashSet<String>> exmp3 = new HashMap<>();
+        HashSet<String> temp5 = new HashSet<>();
+        temp5.add("dwa");
+        temp5.add("reerw");
+        exmp3.put(3, temp5);
+
+        HashMap<Integer, HashSet<String>> exmp4 = new HashMap<>();
+        HashSet<String> temp6 = new HashSet<>();
+        temp6.add("ealkdsr");
+        temp6.add("dwagt");
+        exmp4.put(1, temp6);
+
+        Generator<HashMap<Integer, HashSet<String>>> generator2 = Factory.factoryToJSON(exmp3);
+        System.out.println(generator2.generate(exmp4));
+
+        HashSet<Integer> temp7 = new HashSet<>();
+        temp7.add(5);
+        temp7.add(53);
+        HashMap<String, ArrayList<Integer>> temp8 = new HashMap<>();
         ArrayList<Integer> list1 = new ArrayList<>();
         list1.add(453);
         list1.add(54);
         list1.add(34298);
-        temp2.put("DAWDWD", list1);
+        temp8.put("DAWDWD", list1);
         ArrayDeque<Double> deque1 = new ArrayDeque<>();
         deque1.push(4.64564);
         deque1.push(43.539);
         deque1.push(5.232094);
         String[] arr1 = new String[]{"dwa", "ter3", "wadr3"};
-        MiniClass temp5 = new MiniClass(deque1, arr1);
-        Person integer = new Person("SADW", temp1, temp2, temp5);
+        MiniClass temp9 = new MiniClass(deque1, arr1);
+        Person exmp5 = new Person("SADW", temp7, temp8, temp9);
 
-        HashSet<Integer> temp3 = new HashSet<>();
-        temp3.add(8327);
-        temp3.add(8753);
-        HashMap<String, ArrayList<Integer>> temp4 = new HashMap<>();
+        HashSet<Integer> temp10 = new HashSet<>();
+        temp10.add(8327);
+        temp10.add(8753);
+        HashMap<String, ArrayList<Integer>> temp11 = new HashMap<>();
         ArrayList<Integer> list2 = new ArrayList<>();
         list2.add(5978);
         list2.add(430);
         list2.add(8743);
-        temp4.put("WDAJWDAm,vfsv", list2);
+        temp11.put("WDAJWDAm,vfsv", list2);
         ArrayDeque<Double> deque2 = new ArrayDeque<>();
         deque2.push(4.64564);
         deque2.push(43.539);
         deque2.push(5.232094);
         String[] arr2 = new String[]{"dwa", "ter3", "wadr3"};
-        MiniClass temp6 = new MiniClass(deque2, arr2);
-        Person integer2 = new Person("darge", temp3, temp4, temp6);
+        MiniClass temp12 = new MiniClass(deque2, arr2);
+        Person exmp6 = new Person("darge", temp10, temp11, temp12);
 
-        Generator<Person> generator = Factory.factoryToJSON(integer);
-        System.out.println(generator.generate(integer2));
+        Generator<Person> generator = Factory.factoryToJSON(exmp5);
+        System.out.println(generator.generate(exmp6));
     }
 }
--- src/ru/sber/karimullin/hw_3/Factory/Factory.java
+++ src/ru/sber/karimullin/hw_3/Factory/Factory.java
@@ -14,17 +14,22 @@ import org.jetbrains.annotations.NotNull;
 
 @SuppressWarnings({"unchecked", "StringConcatenationInsideStringBufferAppend"})
 public class Factory {
+    // Если obj == null, то такой вызов невалиден, вроде null не парсится в json
     public static <T> Generator<T> factoryToJSON(@NotNull T obj)
             throws IOException, NoSuchMethodException, InvocationTargetException, InstantiationException,
                    IllegalAccessException, ClassNotFoundException {
         Class<?> clazz = obj.getClass();
+        Counter.clear();
 
+        // Если obj - массив, то мы не сможем его заэкстэндить в объявлении генерируемого класса, поэтому кидаем
+        // исключение (но массивы могут быть внутри кастомных классов)
         if (clazz.isArray()) {
             throw new IllegalArgumentException();
         }
 
         StringBuilder toCompile = new StringBuilder();
         toCompile.append("import ru.sber.karimullin.hw_3.Generator.*;\n");
+        // Если класс принтабельный, то он джавовый. Если нет, то подключаем либу с кастомным классом
         if (clazzIsPrintable(clazz)) {
             toCompile.append("import " + clazz.toString().split(" ")[1] + ";\n");
         } else if (!clazzIsPrintable(clazz) && !clazz.getPackageName().isEmpty()) {
@@ -40,6 +45,7 @@ public class Factory {
         toCompile.append("public String generate(T elem0_1) {\n");
         toCompile.append("StringBuilder output = new StringBuilder();\n");
         toCompile.append("output.append(\"{\\n\");\n");
+        // Если класс принтабельный, то мы здесь же выведем его оглавление в json, потом сами переведем строку на новую
         if (clazzIsPrintable(clazz)) {
             toCompile.append("output.append(\"    \\\"\" + \"" + clazz.getSimpleName() + "\" + \"\\\": \");\n");
             recursiveJSON(clazz, obj, toCompile, 8 + clazz.getSimpleName().length(), 0,
@@ -59,25 +65,43 @@ public class Factory {
         URLClassLoader classLoader = URLClassLoader.newInstance(new URL[]{RuntimeCompiler.getFile().toURI().toURL()});
         Class<?> compiledClass = Class.forName("GeneratorToJSONFrom" + clazz.getSimpleName(),
                 true, classLoader);
+
+        // Тут вылезает unchecked exception, это нормально поскольку класс не существовал на этапе компиляции и его
+        // никак иначе не привести, как supresswarning-ом
         return (Generator<T>) compiledClass.getConstructor().newInstance();
     }
 
+    // Проверка на принтабельность -- джавовый класс с адекватным toString() / коллекция / мапа
     private static boolean clazzIsPrintable(Class<?> clazz) {
         return clazz == String.class || clazz == Integer.class || clazz == Double.class || clazz == Float.class ||
                 clazz == Long.class || clazz == Short.class || clazz == Byte.class || clazz == Date.class ||
-                clazz == File.class || Collection.class.isAssignableFrom(clazz) ||
-                Map.class.isAssignableFrom(clazz) || clazz.isArray();
+                clazz == File.class || Collection.class.isAssignableFrom(clazz) || Map.class.isAssignableFrom(clazz);
     }
 
+    // Проверка на джавовость с адекватным toString()
     private static boolean clazzIsPrimitive(Class<?> clazz) {
         return clazz == String.class || clazz == Integer.class || clazz == Double.class || clazz == Float.class ||
                 clazz == Long.class || clazz == Short.class || clazz == Byte.class || clazz == Date.class ||
                 clazz == File.class;
     }
 
+    // Рекурсивно углубляемся во внутренность объектов, которые выводим.
+    // obj -- параметр, по которому коллекции находят класс, в который далее надо углубиться.
+    // recursionLevelWithSigns -- параметр, нужный чтобы красиво печатать вложенные custom классы
+    // recursionLevel -- параметр, нужный чтобы разграничивать индексы параметров внутри одной ветки рекурсии (иначе
+    // код не скомпилируется)
+    // isOuter -- параметр, по которому мы понимаем что выбирается следующая рекурсивная ветка внутри одного кастомного
+    // класса
+    // isFromMap -- параметр, по которому мы понимаем что вызов пришел из обработчика мапы, по нему мы понимаем
+    // приписывать нам "Key" / "Val" к индексу переменной или нет. Опять же, проблема которую мы так фиксим --
+    // возможность компиляции
+    // isKey -- параметр, по которому мы понимаем, что же в итоге ставить -- "Key" или "Val"
     private static <T> void recursiveJSON(Class<?> clazz, T obj, StringBuilder toCompile, int recursionLevelWithSigns,
                                       int recursionLevel, boolean isOuter, boolean isFromMap, boolean isKey)
             throws InvocationTargetException, IllegalAccessException {
+        // Проверку на clazz == null тут не ставим -- в таком случае просто clazz не primitive. Если obj == null, мы
+        // заходим в тело, так как все необходимое для его работы у нас есть -- требуется только clazz; obj требуется
+        // обработчикам мапы и коллекций для выяснения вложенных классов
         if (clazzIsPrimitive(clazz)) {
             processPrimitive(clazz, toCompile, recursionLevel, isOuter, isFromMap, isKey);
             return;
@@ -106,6 +130,9 @@ public class Factory {
         processCustom(clazz, obj, toCompile, recursionLevelWithSigns, recursionLevel, isOuter, isFromMap, isKey);
     }
 
+    // signal здесь и далее -- стринговый параметр, позволяющий разграничить пространство индексов, чтобы все
+    // скомпилировалось успешно
+    // Стрингу надо обрамлять в двойные ковычки в джейсоне, отсюда if
     private static void processPrimitive(Class<?> clazz, StringBuilder toCompile, int recursionLevel,
                                              boolean isOuter, boolean isFromMap, boolean isKey) {
         if (isOuter) Counter.counterPlus();
@@ -121,6 +148,11 @@ public class Factory {
                                               int recursionLevel, boolean isOuter, boolean isFromMap, boolean isKey)
             throws InvocationTargetException, IllegalAccessException {
         if (isOuter) Counter.counterPlus();
+        // Сначала ищем класс, в который углубимся далее по рекурсии. Если коллекция пустая, мы считаем что
+        // genericClazz == null, и печатаем null, даже если на деле там и имелся какой-то тип. Такой подход разумен,
+        // поскольку в рантайме Class<?> не сохраняют информацию о типах. Но не все, у List например можно достать
+        // параметризацию в рантайме, но вот у HashSet<> например с этим проблемы.
+        // Флаг gotIt здесь нужен чтобы единожды зайти в цикл, и не обходить всю коллекцию
         Class<?> genericClazz = null;
         Collection<?> collection = (Collection<?>) obj;
         Iterator<?> iterator = collection.iterator();
@@ -135,9 +167,13 @@ public class Factory {
             return;
         }
 
+        // signal и signal_plus1 нужны здесь, чтобы склеивать предыдущую итерацию рекурсии и текущую
         String signal = Counter.getCounter(recursionLevel, isFromMap, isKey);
         String signal_plus1 = Counter.getCounter(recursionLevel + 1, isFromMap, isKey);
 
+        // Заводим флаг вхождения в цикл, по которому если что потом сотрем лишнюю запятую, далее приводим переданный
+        // elem.._.. к collection, откуда достаем итератор, по которому итерируемся в цикле, приводя явно элементы под
+        // итератором к установленному типу. Далее вставляем рекурсивно код, печатающий обработку вложенных классов
         toCompile.append("output.append(\"[\");\n");
         toCompile.append("boolean isWhileWorked" + signal + " = false;\n");
         toCompile.append("Collection<?> collection" + signal + " = (Collection<?>) elem" + signal + ";\n");
@@ -162,17 +198,25 @@ public class Factory {
         if (isOuter) Counter.counterPlus();
         Class<?> genericClazz = clazz.getComponentType();
 
+        // Если переданный массив пуст, мы говорим что не будем парсить все следующие объекты. Что, конечно, не совсем
+        // правильно, ведь тип genericClazz через массив уже установлен, но тогда учитывание специфики массива
+        // (что нам необязательно иметь объекты внутри массива чтобы доподлинно сказать что в нем лежит) сильно бы
+        // отразилось на всех прочих обработчиках. Пришлось бы вводить флаги прихода из массива, передавать весь трейс
+        // классов которые были установленны таким образом. Поэтому будем считать так
         Object[] tempArray = (Object[]) obj;
         if (tempArray.length == 0) {
             toCompile.append("output.append(\"null\");\n");
             return;
         }
 
+        // arrayType надо спарсить в строку, учитывая [L в начале и ; на конце
         String signal = Counter.getCounter(recursionLevel, isFromMap, isKey);
         String signal_plus1 = Counter.getCounter(recursionLevel + 1, isFromMap, isKey);
         String arrayType = clazz.toString().split(" ")[1];
         arrayType = arrayType.substring(2, arrayType.length() - 1);
 
+        // Здесь подход к итерации другой. Мы кастим пришедший элемент к массиву, и в цикле for кастим элемент массива
+        // к установленному типу, после чего вызываем рекурсивно код, обрабатывающий установленный тип.
         toCompile.append("output.append(\"[\");\n");
         toCompile.append("boolean isWhileWorked" + signal + " = false;\n");
         toCompile.append(arrayType + "[] array" + signal + " = (" + arrayType + "[]) elem" + signal + ";\n");
@@ -190,6 +234,7 @@ public class Factory {
         toCompile.append("output.append(\"]\");\n");
     }
 
+    // Аналогично пункту про Collection
     private static <T> void processMap(T obj, StringBuilder toCompile, int recursionLevelWithSigns,
                                               int recursionLevel, boolean isOuter, boolean isFromMap, boolean isKey)
             throws InvocationTargetException, IllegalAccessException {
@@ -243,6 +288,9 @@ public class Factory {
         toCompile.append("output.append(\"}\");\n");
     }
 
+    // Если флаг isOuter не установлен, значит мы пришли из другого кастомного класса, или из коллекции в которой
+    // лежали кастомные классы. Тогда чтобы красиво вывелось надо поставить перевод строки, иначе углубить индекс
+    // чтобы отличаться от других кастомных классов в индексном пространстве
     private static <T> void processCustom(Class<?> clazz, T obj, StringBuilder toCompile, int recursionLevelWithSigns,
                                        int recursionLevel, boolean isOuter, boolean isFromMap, boolean isKey)
             throws InvocationTargetException, IllegalAccessException {
@@ -252,6 +300,7 @@ public class Factory {
         } else {
             toCompile.append("output.append(\"\\n\");\n");
         }
+        // Старый индекс всем полям кастомного класса нужен одинаковый, выносим его из цикла
         String signal = Counter.getCounter(recursionLevel, isFromMap, isKey);
         for (Method method : methods) {
             if (method.getName().startsWith("get") && method.getParameterCount() == 0 &&
@@ -280,6 +329,9 @@ public class Factory {
         }
     }
 
+    // Нам нужно разграничить индексы переменных в сгенерированном коде по веткам рекурсии, по вызову в Custom блоке,
+    // и по Key / Map парам. Класс статический, значит надо сбрасывать счетчик, разграничивающий вызовы в Custom блоке
+    // при новом вызове factoryToJSON().
     static class Counter {
         private static int counter = 0;
 
@@ -297,6 +349,9 @@ public class Factory {
             }
             return recursionLevel + "_" + counter;
 
+        }
+        public static void clear() {
+            counter = 0;
         }
     }
 }
